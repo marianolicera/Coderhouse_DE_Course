ec6e24f2da84776fa338ca100ed964442046e0d4
Mariano Licera <marianolicera3@gmail.com>
2024-04-09 19:11:18 -0300

    moved python files to src folder

---
+++ src/config.py
@@ -0,0 +1,6 @@
+redshift_pwd='P7mQd0P66O'
+api_key='behGGPCV9pYyEfwrUqPfIjb4axaqJZ0a'
+redshift_host = 'data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com'
+redshift_database = 'data-engineer-database'
+redshift_user = 'marianolicera3_coderhouse'
+redshift_port = 5439
