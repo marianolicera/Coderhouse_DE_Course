ce9ed02e96222346957b62cabc9c99b050402b0e
Mariano Licera <marianolicera3@gmail.com>
2024-04-11 15:18:33 -0300

    added api weather dag

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
+from src.main import etl_main_function
 
 default_args={
     'owner' : 'Mariano',
@@ -9,7 +10,7 @@ default_args={
 },
 
 with DAG(
-    'Coderhouse_DE_Course',
+    dag_id = 'a_etl_process_weather',
     default_args=default_args,
     description = 'Dag para obtener datos del clima en 150 ciudades y subirlas a una BD',
     start_date=datetime(2024, 4, 11, 2), #Se inicia el 11 de abril de 2024 y se ejecuta diariamente,
@@ -17,7 +18,17 @@ with DAG(
     catchup = False,
     tags = ['Course'],
 ) as dag:
-    task1 = PythonOperator(
-    task_id = 'weather_api ejecute',
-    python_callable =  main()
+
+    def send_mail_function():
+        print('Sending mail with OK status')
+
+    etl_process = PythonOperator(
+        task_id = 'weather_api ejecute',
+        python_callable =  etl_main_function()
     )
+    mail_sending = PythonOperator(
+        task_id = 'sending mail with OK status',
+        python_callable = send_mail_function()
+    )
+
+    etl_process >> mail_sending
