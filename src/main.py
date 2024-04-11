ca54acf8d379a1d9201d489e87927dfc84329391
Mariano Licera <marianolicera3@gmail.com>
2024-04-11 12:46:04 -0300

    added main.py file to execute ETL process

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from extract import extract_data
+from transform import transform_data
+from load import load_data
+from config import api_key, redshift_host, redshift_database, redshift_user, redshift_pwd, redshift_port
+
+# Data extraction
+data_extracted = extract_data(api_key)
+
+# Data transformation
+data_transformed = transform_data(data_extracted)
+
+# Data loading to redshift
+load_data(data_transformed, redshift_host, redshift_database, redshift_user, redshift_pwd, redshift_port)
+
+print("ETL process successfully completed.")
