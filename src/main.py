685b060a0f3adbdb0726f6e7094514d39e56e4d8
Mariano Licera <marianolicera3@gmail.com>
2024-04-11 15:18:18 -0300

    added function in main.py

--- src/main.py
+++ src/main.py
@@ -3,13 +3,14 @@ from transform import transform_data
 from load import load_data
 from config import api_key, redshift_host, redshift_database, redshift_user, redshift_pwd, redshift_port
 
-# Data extraction
-data_extracted = extract_data(api_key)
+def etl_main_function():
+    # Data extraction
+    data_extracted = extract_data(api_key)
 
-# Data transformation
-data_transformed = transform_data(data_extracted)
+    # Data transformation
+    data_transformed = transform_data(data_extracted)
 
-# Data loading to redshift
-load_data(data_transformed, redshift_host, redshift_database, redshift_user, redshift_pwd, redshift_port)
+    # Data loading to redshift
+    load_data(data_transformed, redshift_host, redshift_database, redshift_user, redshift_pwd, redshift_port)
 
-print("ETL process successfully completed.")
+    print("ETL process successfully completed.")
